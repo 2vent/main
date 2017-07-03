9cd54120d74f4f89324504f0d27d9a047e94c39c
이재원 <kjd99001@naver.com>
2017-07-03 17:04:04 +0900

    유저인포 아이템인포

---
+++ .gitconfig/app/src/main/java/com/example/win/a2vent/user_iteminfo.java
@@ -0,0 +1,17 @@
+package com.example.win.a2vent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017-07-03.
+ */
+
+public class user_iteminfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userinfo);
+
+    }
+}
