c4dc34fbef6b0515bd94bdbbbaa09a11b3f8cfa1
EUNJAESHIN <ejshin0214@gmail.com>
2017-07-14 13:57:04 +0900

    ㅁㄴㅇㄹ

--- .gitconfig/app/src/main/java/com/example/win/a2vent/owner_Event_Main.java
+++ .gitconfig/app/src/main/java/com/example/win/a2vent/owner_Event_Main.java
@@ -1,7 +1,9 @@
 package com.example.win.a2vent;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -12,10 +14,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by win on 2017-07-06.
@@ -23,7 +36,11 @@ import java.util.ArrayList;
 
 public class owner_Event_Main extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private int nType=1;
+    private static String TAG = "owner_Event_Main";
+    private static final String TAG_JSON = "company";
+    String mJsonString;
+    private int nType = 1;
+
     Context mContext;
     RecyclerView recyclerView;
     owner_Event_Adapter Adapter;
@@ -45,7 +62,7 @@ public class owner_Event_Main extends AppCompatActivity implements NavigationVie
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(nType == 1) {
+                if (nType == 1) {
                     Intent intent_eventform = new Intent(getBaseContext(), owner_Event_Addevent.class);
                     startActivity(intent_eventform);
                 } else {
@@ -96,12 +113,12 @@ public class owner_Event_Main extends AppCompatActivity implements NavigationVie
         int id = item.getItemId();
 
         if (id == R.id.my_company) {
-            nType=0;
+            nType = 0;
             ArrayList<owner_Addstore_Item> items = new ArrayList<owner_Addstore_Item>();
-            items.add(new owner_Addstore_Item("1", "SKT", "경북 구미시 우리집", "10101", "kjd99002", "smt001", "s"));
-            items.add(new owner_Addstore_Item("2", "LG", "경북 구미시 우리집", "10101", "kjd99001", "smt001", "s"));
-            items.add(new owner_Addstore_Item("3", "지앤케이", "대구 동대구역 앞", "10101", "kjd99003", "smt001", "s"));
-            items.add(new owner_Addstore_Item("4", "모름", "경북 구미시 우리집", "10101", "kjd99004", "smt001", "s"));
+//            items.add(new owner_Addstore_Item("1", "SKT", "경북 구미시 우리집", "10101", "kjd99002", "smt001", "s"));
+//            items.add(new owner_Addstore_Item("2", "LG", "경북 구미시 우리집", "10101", "kjd99001", "smt001", "s"));
+//            items.add(new owner_Addstore_Item("3", "지앤케이", "대구 동대구역 앞", "10101", "kjd99003", "smt001", "s"));
+//            items.add(new owner_Addstore_Item("4", "모름", "경북 구미시 우리집", "10101", "kjd99004", "smt001", "s"));
 
             layoutManager = new LinearLayoutManager(this);
             recyclerView.setLayoutManager(layoutManager);
@@ -110,7 +127,7 @@ public class owner_Event_Main extends AppCompatActivity implements NavigationVie
             recyclerView.setAdapter(Adapter_com);
 
         } else if (id == R.id.on_event) {
-            nType=1;
+            nType = 1;
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "문", "ad"));
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "동", "ad"));
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "이이이이이이", "ad"));
@@ -127,7 +144,7 @@ public class owner_Event_Main extends AppCompatActivity implements NavigationVie
             recyclerView.setAdapter(Adapter);
 
         } else if (id == R.id.temp_event) {
-            nType=1;
+            nType = 1;
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "문", "ad"));
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "동", "ad"));
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "이이이이이이", "ad"));
@@ -144,7 +161,7 @@ public class owner_Event_Main extends AppCompatActivity implements NavigationVie
             recyclerView.setAdapter(Adapter);
 
         } else if (id == R.id.end_event) {
-            nType=1;
+            nType = 1;
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "문", "ad"));
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "동", "ad"));
             event_list.add(new owner_Event_Item(1, 1, 1, "fiwj", 5000, 4000, 4, "20170707", "20170707", "5018", "5018", 0, 1, 59, 59, 1, "대구", "3", "던파", "이이이이이이", "ad"));
@@ -165,5 +182,106 @@ public class owner_Event_Main extends AppCompatActivity implements NavigationVie
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class GetData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(owner_Event_Main.this,
+                    "Please Wait", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            Log.d(TAG, "response  - " + result);
+
+            if (result == null) {
+
+            } else {
+
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+
+            try {
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString().trim();
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+
+    private void showResult() {
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject item = jsonArray.getJSONObject(i);
+
+                String com_number = item.getString("com_number");
+                String com_name = item.getString("com_name");
+                String com_addr = item.getString("com_addr");
+                String com_category = item.getString("com_category");
+                String com_manager = item.getString("com_manager");
+                String com_URI = item.getString("com_URI");
+                String id = item.getString("id");
+
+            }
+
+
+        } catch (JSONException e) {
+            Log.d(TAG, "showResult : ", e);
+        }
+
     }
 }
--- .gitconfig/app/src/main/java/com/example/win/a2vent/user_Event_Main.java
+++ .gitconfig/app/src/main/java/com/example/win/a2vent/user_Event_Main.java
@@ -20,10 +20,10 @@ public class user_Event_Main extends AppCompatActivity {
     private static String TAG = "JSON으로 데이터 가져오기";
 
     Context mContext;
-    RecyclerView.Adapter rAdapter1,rAdapter2;
-    RecyclerView rView1,rView2;
+    RecyclerView.Adapter rAdapter1, rAdapter2;
+    RecyclerView rView1, rView2;
     String mJsonString;
-    ArrayList category_all,category_fashion;
+    ArrayList category_all, category_fashion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,122 +89,5 @@ public class user_Event_Main extends AppCompatActivity {
         tabSpec5.setIndicator("문화");
         tabHost.addTab(tabSpec5);
     }
-/*
-    private class GetData extends AsyncTask<String, Void, String> {
-        ProgressDialog progressDialog;
-        String errorString = null;
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            progressDialog = ProgressDialog.show(user_event_main.this,
-                    "Please Wait", null, true, true);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            progressDialog.dismiss();
-            mTextViewResult.setText(result);
-            Log.d(TAG, "response  - " + result);
-
-            if (result == null) {
-
-                mTextViewResult.setText(errorString);
-            } else {
-
-                mJsonString = result;
-                showResult();
-            }
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String serverURL = params[0];
-
-            try {
-                URL url = new URL(serverURL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-                httpURLConnection.setReadTimeout(5000);
-                httpURLConnection.setConnectTimeout(5000);
-                httpURLConnection.connect();
-
-                int responseStatusCode = httpURLConnection.getResponseCode();
-                Log.d(TAG, "response code - " + responseStatusCode);
-
-                InputStream inputStream;
-                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
-                    inputStream = httpURLConnection.getInputStream();
-                } else {
-                    inputStream = httpURLConnection.getErrorStream();
-                }
-
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                StringBuilder sb = new StringBuilder();
-                String line;
-
-                while ((line = bufferedReader.readLine()) != null) {
-                    sb.append(line);
-                }
-
-                bufferedReader.close();
-
-                return sb.toString().trim();
-
-            } catch (Exception e) {
-
-                Log.d(TAG, "InsertData: Error ", e);
-                errorString = e.toString();
-
-                return null;
-            }
-
-        }
-    }
-
-    private void showResult() {
-        try {
-            JSONObject jsonObject = new JSONObject(mJsonString);
-            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-
-                JSONObject item = jsonArray.getJSONObject(i);
-
-                String id = item.getString(TAG_ID);
-                String name = item.getString(TAG_NAME);
-                String address = item.getString(TAG_BIRTH);
-
-                HashMap<String, String> hashMap = new HashMap<>();
-
-                hashMap.put(TAG_ID, id);
-                hashMap.put(TAG_NAME, name);
-                hashMap.put(TAG_BIRTH, address);
-
-                mArrayList.add(hashMap);
-            }
-
-            ListAdapter adapter = new SimpleAdapter(
-                    ListActivity.this, mArrayList, R.layout.item_list,
-                    new String[]{TAG_ID, TAG_NAME, TAG_BIRTH},
-                    new int[]{R.id.textView_list_id, R.id.textView_list_name, R.id.textView_list_birth}
-            );
-
-
-            mlistView.setAdapter(adapter);
-
-        } catch (JSONException e) {
-
-            Log.d(TAG, "showResult : ", e);
-        }
-
-    }
-
-*/
 }
